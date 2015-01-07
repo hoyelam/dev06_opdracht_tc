b555dbe1e7c8a6eff7af31d6bd9adaaaba7a205a
Hoye <hoyelam96@gmail.com>
2015-01-07 14:42:28 +0100

    test 24

--- CIStiften-master/Arrays/src/test/java/CardTest1.java
+++ CIStiften-master/Arrays/src/test/java/CardTest1.java
@@ -7,7 +7,7 @@ public class CardTest1 {
     @Test
     public void testCompareTo() {
         Card c1 = new Card(Number.AAS, Suit.HARTEN);
-        //Card c2 = new Card(Number.TWEE, Suit.SCHOPPEN);
+        Card c2 = new Card(Number.TWEE, Suit.SCHOPPEN);
 
         assertTrue(c1.compareTo(c1) == 0);
 
